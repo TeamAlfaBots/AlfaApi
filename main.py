0dfc0eb74932c59ff5b0733cdeca05ece8dbb956
TeamAlfaBots <thedeveloper413@gmail.com>
2026-03-18 14:59:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ from yt_dlp import YoutubeDL
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "AlfaApi running 🚀"}
-
 @app.get("/search")
 def search(title: str):
-    ydl_opts = {
-        "format": "bestaudio",
-        "quiet": True,
-        "noplaylist": True,
-    }
-
-    with YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(f"ytsearch:{title}", download=False)
-        video = info["entries"][0]
-
-        return {
-            "link": video["url"],
-            "title": video["title"],
-            "duration": video["duration"],
-            "thumbnail": video["thumbnail"]
+    try:
+        ydl_opts = {
+            "format": "bestaudio",
+            "quiet": True,
+            "noplaylist": True,
         }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(f"ytsearch:{title}", download=False)
+            video = info["entries"][0]
+
+            return {
+                "link": video["url"],
+                "title": video["title"],
+                "duration": video["duration"],
+                "thumbnail": video["thumbnail"]
+            }
+
+    except Exception as e:
+        return {"error": str(e)}
